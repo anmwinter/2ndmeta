b1fab362684531247b7adbf951b0ea518487d3d2
Ara <ghashsnaga@gmail.com>
2013-03-29 11:06:53 -0600

    Added custom_fasta.py
    This is a bit of python code using Biopython to read large NCBI
    formatted bacteria fasta files. Once the file is read in the code spits
    out a genus level fasta file.

---
+++ custom_fasta.py
@@ -0,0 +1,81 @@
+from Bio import SeqIO
+import re
+
+
+################################################################################
+#
+#Here we start with a function to access the data in the fasta file
+#and to create a custom fasta that reads like:
+#> some_genus_name
+#sequence
+#
+################################################################################
+
+
+
+def custom_fasta(fasta_file):
+   
+    #out_list = open("list"+fasta_file+".txt","w+")
+    out_custom = open("custom_fasta.fasta","w+")
+
+    
+    for seq_record in SeqIO.parse(fasta_file,"fasta"):
+
+#r is a regular expression. Here I am searching for a white space (\s) followed
+#by some combination of letters and numbers which is the genus
+        genus_name = extract(seq_record.description, r'\s\w+')
+
+#r is a regular expression. Here I am searching for a some number of capital
+#letters and underscore [A-Z_] plus some numbers \d+, followed by a period,
+#and some numbers \d+. This will give you the accession number
+        acc_num = extract(seq_record.description, r'[A-Z_]+\d+.\d+')
+        genus = str(genus_name)
+        
+        
+        out_custom.write(">"+genus.strip()+" "+acc_num+"\n"+str(seq_record.seq)+"\n")
+
+    out_custom.close()
+
+################################################################################
+#
+#The code that follows takes your custom fasta file and removes duplicates 
+#by name. You will end up with a fasta file at the genus level. There is a
+#seperate file call genera_list which lists each genera and it's accesion number
+#
+################################################################################
+    sequences={}
+    for seq_record in SeqIO.parse("custom_fasta.fasta", "fasta"):
+        acc = extract(seq_record.description, r'[A-Z_]+\d+.\d+')
+        sequence=seq_record.id
+        if sequence not in sequences:
+            sequences[sequence]=str(seq_record.seq)
+
+    output_file=open("clear_"+fasta_file,"w+")
+    out_genera=open("genera_list"+fasta_file,"w+")
+
+    for sequence in sequences:
+            output_file.write(">"+sequence+"\n"+sequences[sequence]+"\n")
+            out_genera.write(sequence+"\t"+acc+"\n")
+    output_file.close()
+    out_genera.close()
+
+    
+        
+#####
+#
+#This function sets up the reguar expression routine for pulling out information
+#in the fasta file. This will convert the extracted pattern to a string
+#
+#####
+def extract(src, pattern):
+     src_str = str(src)
+     matchObj = re.search(pattern, src_str)
+     if matchObj:
+          return matchObj.group()
+     else:
+          return "NONE"
+
+
+
+custom_fasta("fungi.fasta")
+
